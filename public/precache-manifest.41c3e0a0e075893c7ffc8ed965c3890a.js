f3871f294216f66544db587ee9ce553e18b45d0f
babylee2002 <babylee2002@gmail.com>
2018-10-09 21:04:27 -0400

    setup complete

---
+++ public/precache-manifest.41c3e0a0e075893c7ffc8ed965c3890a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8aa3f9d648caa16063ea",
+    "url": "/static/js/main.8aa3f9d6.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "8aa3f9d648caa16063ea",
+    "url": "/static/css/main.f16f5d0c.chunk.css"
+  },
+  {
+    "revision": "98046f3e936370ad0e25a7f4c6cc142a",
+    "url": "/index.html"
+  }
+];
